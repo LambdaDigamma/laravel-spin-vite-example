c9537e6a4c95da25b2e5eed1500687f0a131da37
Lennart Fischer <github@lambdadigamma.com>
2024-05-25 18:18:34 +0200

    added vite spin configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+const env = loadEnv('', process.cwd());
+
+const host = env.VITE_HOST;
 
 export default defineConfig({
     plugins: [
+        basicSsl(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        host,
+        hmr: {
+            host,
+            clientPort: 443
+        },
+    },
 });
